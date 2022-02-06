40b930a6c2815ad2d974ae820b4e309bd291ad4a
nelsonnyan2001 <zwenyantoe@my.jcu.edu.au>
2022-02-06 11:09:00 -0800

    Suu-Sann page

---
+++ README.md
@@ -0,0 +1,7 @@
+# Frontend for Thate Pun Hub's web redesign.
+
+It's just HTML with a Tailwind listener.
+
+Scripts are in `package.json`.
+
+Deployment on Netlify.
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+module.exports = {
+    content: [],
+    theme: {
+        extend: {
+            fontFamily: {
+                'sans': ['Poppins', 'Helvetica', 'Arial', 'sans-serif']
+
+            }
+        },
+    },
+    content: {
+        enabled: true,
+        content: ['./src/**/*.html']
+    },
+    plugins: [],
+}
